baf9a61527aeb4a57c708286669ed41bc7598f99
Oleg Prokofiev <89846796+prokoleg@users.noreply.github.com>
2023-09-17 15:08:57 +0300

    Create config.php

---
+++ qr_VCARD/config.php
@@ -0,0 +1,23 @@
+<?php
+// script by Prokofiev Oleg info@blanet.ru ©2022
+//
+// Права на данный скрипт пренадлежает его автору.
+// Модификация и использование данного скрипта разрешается только с разрешения
+// его автора Прокофьева Олега (info@blanet.ru)
+// Незаконное использование и распространение скрипта ЗАПРЕЩЕНО
+// Скрипт распространяется по лицензии MIT
+
+// Адрес сайта
+$url = ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+$url = explode('?', $url);
+$url = $url[0];
+define('HOME', $url);
+
+// Константы
+define('REQUEST', $_SERVER['QUERY_STRING']);
+
+// Подключение классов
+foreach (glob('inc/classes/*.php') as $filename)
+{
+    include $filename;
+}
