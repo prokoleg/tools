89ab15f92025f507c7db66c6c790c6d714eac56b
Oleg Prokofiev <89846796+prokoleg@users.noreply.github.com>
2023-09-17 15:11:20 +0300

    Add files via upload

---
+++ qr_VCARD/index.php
@@ -0,0 +1,90 @@
+<?php
+// script by Prokofiev Oleg info@blanet.ru ©2022
+//
+// Права на данный скрипт пренадлежает его автору.
+// Модификация и использование данного скрипта разрешается только с разрешения
+// его автора Прокофьева Олега (info@blanet.ru)
+// Незаконное использование и распространение скрипта ЗАПРЕЩЕНО
+// Скрипт распространяется по лицензии MIT
+
+require_once __DIR__ . '/config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="" prefix="og: http://ogp.me/ns#">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+    <meta http-equiv="Last-Modified" content="San, 11 Sen 2022 12:45:26 GMT">
+<!-- OpenGraph -->
+	<meta property="og:locale" content="ru_RU" />
+	<meta property="og:image" content="" />
+	<meta property="og:type" content="website" />
+	<meta property="og:site_name" content="" />
+	<meta property="og:title" content="" />
+	<meta property="og:url" content="" />
+	<meta property="og:description" content="" />
+<!-- OpenGraph -->
+	<link rel="alternate" media="only screen and (max-width: 991px)" href="" />
+	<link rel="icon" href="../img/favicon.svg" sizes="any" type="image/svg+xml">
+	<link rel="apple-touch-icon" href="../img/favicon.svg" type="image/svg+xml">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+  	<link rel="stylesheet" href=""/>
+	<link rel="stylesheet" href=""/>
+	<link rel="canonical" href=""/>
+	<title></title>
+</head>
+<body>
+<div class="container">
+
+<?php
+if (!$_GET) {
+?>
+<h2>Введите данные</h2>
+<form method="get">
+<div class="input-group flex-wrap: wrap">
+  <span class="input-group-text" id="addon-wrapping">Имя</span>
+  <input type="text" class="form-control" placeholder="Имя пользователя" aria-label="Имя пользователя" aria-describedby="addon-wrapping" name="firstname" size="55">
+</div>
+<br />
+<div class="input-group flex-wrap: wrap">
+  <span class="input-group-text" id="addon-wrapping">Фамилия</span>
+  <input type="text" class="form-control" placeholder="Имя пользователя" aria-label="Имя пользователя" aria-describedby="addon-wrapping" name="lastname">
+</div>
+<br />
+<div class="input-group flex-nowrap">
+  <span class="input-group-text" id="addon-wrapping">Телефон</span>
+  <input type="text" class="form-control" placeholder="Имя пользователя" aria-label="Имя пользователя" aria-describedby="addon-wrapping" name="phone">
+</div>
+<br />
+<div class="input-group flex-nowrap">
+  <span class="input-group-text" id="addon-wrapping">Email</span>
+  <input type="text" class="form-control" placeholder="Имя пользователя" aria-label="Имя пользователя" aria-describedby="addon-wrapping" name="email">
+</div>
+<br />
+<div class="input-group flex-nowrap">
+  <span class="input-group-text" id="addon-wrapping">Сайт</span>
+  <input type="text" class="form-control" placeholder="Имя пользователя" aria-label="Имя пользователя" aria-describedby="addon-wrapping" name="url">
+</div>
+<br />
+<button>Lets go!</button>
+<code>ВНИМАНИЕ! Все поля, кроме адреса сайта и почты обязательны к заполнению</code>
+</form>
+<?php
+} elseif ($_GET['firstname'] == null || $_GET['lastname'] == null || $_GET['email'] == null) {
+	echo '<code>ВНИМАНИЕ! Все поля, кроме адреса сайта и почты обязательны к заполнению</code><br /><a href="'.HOME.'">Повторить</a>';
+} else {
+?>
+<h2>Ваш QR-код</h2>
+<?php
+echo '<img src="'.HOME.'qr.php?size=25&firstname='.$_GET['firstname'].'&lastname='.$_GET['lastname'].'&phone='.$_GET['phone'].'&url='.$_GET['url'].'&email='.$_GET['email'].'&api=bhTBYrt45vrb6vyrfDvg" width=150>';
+
+echo '<br /><a href="'.HOME.'">Назад</a>';
+}
+?>
+</div>
+</body>
+</html>
---
+++ qr_VCARD/qr.php
@@ -0,0 +1,102 @@
+<?php
+
+/*
+API для создания QR-кода визитки VCARD
+
+Формат запроса: http://test.site/qr/?size=РАЗМЕР&firstname=ИМЯ&lastname=ФАМИЛИЯ&phone=ТЕЛЕФОН&url=URL&email=EMAIL&api=API
+
+Формат данных:
+РАЗМЕР		- 50, 75, 100 (процентное соотношение качества картинки)
+ИМЯ			- имя
+ФАМИЛИЯ		- фамилия
+ТЕЛЕФОН		- номер телефона в формате 79998887766
+URL			- адрес сайта в формате site.ru
+EMAIL		- адрес почты
+API			- Ваш секретный ключ (по-умолчанию: bhTBYrt45vrb6vyrfDvg)
+
+ВНИМАНИЕ! Все поля, кроме размера, адреса сайта и почты обязательны к заполнению
+*/
+
+require_once __DIR__ . '/phpqrcode/qrlib.php';
+require_once __DIR__ . '/config.php';
+
+$api = 'bhTBYrt45vrb6vyrfDvg';
+
+/* Временный каталог для картинки*/
+$tmpimagedir = '/tmp';
+$host = $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+$output_true = 'Access not denied!';
+$output_fail = 'Access denied!';
+
+if ($_GET) {
+/*Создание текста для QR-кода*/
+
+$firstname = isset($_GET['firstname']) ? $_GET['firstname'] : '';
+$lastname = isset($_GET['lastname']) ? $_GET['lastname'] : '';
+$phone = isset($_GET['phone']) ? $_GET['phone'] : '';
+$photo = 'https://vinil-spb.ru/image/catalog/avatars/qIR0igjmUv4111.jpg';
+$photo1 = 'https://vinil-spb.ru/image/catalog/avatars/blogger.jpg';
+$email = (isset($_GET['email']) ? $_GET['email'] : null);
+$api_key = (isset($_GET['api'])) ? $_GET['api'] : null;
+$img = '<img src="' . $photo . '">';
+$img_1 = '<img src="' . $photo1 . '">';
+
+
+if (!isset($url)) {
+	$url = 'https://blanet.ru';
+} else {
+	$url = 'https://' . $_GET['url'];
+}
+
+$text  = 'BEGIN:VCARD' . "\n";
+$text .= 'N: ' . $firstname . ';' .$lastname. "\n";
+$text .= 'TEL;WORK;VOICE;CELL: +' . $phone . "\n";
+$text .= 'URL: ' . $url . "\n";
+$text .= 'EMAIL;TYPE=INTERNET: ' . $email . "\n";
+$text .= 'PHOTO;URI:' . $photo . "\n";
+$text .= 'LOGO;VALUE=uri:' . $photo . "\n";
+$text .= 'END:VCARD';
+
+} else {
+	$text = ' Обязательные поля не заполены. Карточка контакта не загружена. Пожалуйста, заполните недостающие поля или создайте правильный запрос к нашему API'. "\n";
+}
+
+/* Проверяем введено-ли качество создаваемой картинки */
+$size = isset($_GET['size']) ? $_GET['size'] : '25';
+$size = ($size == 50) ? 'M' : (($size == 75) ? 'Q' : (($size == 100) ? 'H' : 'L'));
+
+/* Генерация QR-кода во временный файл */
+QRcode::png($text, __DIR__ . $tmpimagedir . '/'.$hashid->getHashImg().'.png', $size, 6, 2);
+ 
+/* Замена белых пикселей на прозрачный */
+$im = imagecreatefrompng(__DIR__ . $tmpimagedir . '/'.$hashid->getHashImg().'.png');
+$width = imagesx($im);
+$height = imagesy($im);
+ 
+$bg_color = imageColorAllocate($im, 0, 0, 0);
+imagecolortransparent ($im, $bg_color);
+ 
+for ($x = 0; $x < $width; $x++) {
+	for ($y = 0; $y < $height; $y++) {
+		$color = imagecolorat($im, $x, $y);
+		if ($color == 0) {
+			imageSetPixel($im, $x, $y, $bg_color);
+		}
+	}
+}
+
+if (isset($api_key) && $api_key == $api) {
+
+/* Если нет имени, фамилии и телефона, то выводим УПС */
+if ($firstname == true && $lastname == true && $phone == true) {
+
+/* Вывод в браузер */
+header('Content-Type: image/x-png');
+imagepng($im);
+
+// Удаляем временный файл изображения
+unlink('tmp/'.$hashid->getHashImg().'.png');
+}
+} else {
+	echo ($host == $_SERVER['SERVER_NAME']) ? $output_true : ((isset($_GET['api_ver']) && $_GET['api_ver'] === '1') ? $img : (($_GET['api_ver'] === '2') ? $img_1 : $output_fail));
+}
